ad37d9fa98c32f388ecb38dc27a95b52af531365
Giulio Dioguardi <giulio.dioguardi@alten.nl>
2015-10-27 09:17:43 +0100

    Added unit test for Money Calculator

---
+++ GeldCalculatorUnitTest/MoneyCalculatorTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Geld_Calculator;
+using System.Windows.Forms;
+
+namespace GeldCalculatorUnitTest
+{
+    [TestClass]
+    public class MoneyCalculatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MoneyCalculator calc = new MoneyCalculator();
+            calc.calculate(new string[15] { "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1" });
+            Assert.AreEqual(calc.getTotal(), 888.88, "Wrong total");
+        }
+    }
+}
